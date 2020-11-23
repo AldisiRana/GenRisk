b9a155c17a2af26a9c8490526387a1c29fc8b663
AldisiRana <35561773+AldisiRana@users.noreply.github.com>
2020-11-23 20:31:23 +0100

    Update plot_script.R

--- scripts/plot_script.R
+++ scripts/plot_script.R
@@ -45,7 +45,7 @@ manhattan(complete_df,chr='Chr', bp="Start", snp=opt$genescol_1, p=opt$pvalcol,
           ylim = c(0, -log10(1e-06)), chrlabs = NULL,
           suggestiveline = -log10(1e-03), genomewideline = -log10(1e-05), logp = TRUE, main=opt$pvals_file, highlight=T, annotatePval=1, annotateTop=T)
 dev.off()
-lambda=median(qchisq(complete_df$pval, df=1, lower.tail=FALSE)) / qchisq(0.5, 1)
+lambda=median(qchisq(complete_df$pvalcol, df=1, lower.tail=FALSE)) / qchisq(0.5, 1)
 jpeg(opt$qq_output, res=300, width = 6, height = 6, units = 'in')
 qq(complete_df$pval,main=as.character(lambda))
 dev.off()
