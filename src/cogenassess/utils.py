680159d8182e56a47ff536becbd1373c35bb04f1
AldisiRana <35561773+AldisiRana@users.noreply.github.com>
2021-03-24 11:01:16 +0100

    add function for prediction model

--- src/cogenassess/utils.py
+++ src/cogenassess/utils.py
@@ -5,9 +5,14 @@ import re
 import subprocess
 
 import pandas as pd
+import pycaret.classification as cl
+import pycaret.regression as pyreg
+from pycaret.utils import check_metric
+import requests
 import numpy as np
 from scipy.stats import beta
 from tqdm import tqdm
+import urllib.request as urllib
 
 
 def get_gene_info(
@@ -82,3 +87,77 @@ def plink_process(*, genes_folder, plink, bed, bim, fam):
             " --fam " + fam + " --extract " + v_file + " --score " + w_file + " 1 2 3 sum --out " +
             os.path.join(genes_folder, gene), shell=True
         )
+
+
+def get_prs(
+    *,
+    prs_id,
+    bed,
+    bim,
+    fam,
+    plink,
+    output_file,
+):
+    resp = requests.get('https://www.pgscatalog.org/rest/score/%s' % prs_id)
+    prs_info = resp.json()
+    if resp.status_code != 200 or not prs_info:
+        raise Exception('The PRS score might be wrong!')
+    url = prs_info['ftp_scoring_file']
+    prs_file = prs_id+'.gz'
+    urllib.urlretrieve(url, prs_file)
+    # unpack file
+    # find a way to change rsID to snpID
+    p = subprocess.call(
+        plink + " --bed " + bed + " --bim " + bim + " --fam " + fam + " --score " + prs_file + " 1 2 3 sum --out " +
+        output_file, shell=True
+    )
+
+
+def create_model(
+    *,
+    output_folder,
+    model_name='final_model',
+    model_type='reg',
+    y_col,
+    imbalanced=True,
+    normalize=True,
+    folds=10,
+    training_set,
+    testing_set=None,
+    test_size=0.25,
+    metric=None,
+):
+    df = pd.read_csv(training_set, sep='/t', index_col=False)
+    if testing_set:
+        test = pd.read_csv(testing_set, sep='/t', index_col=False)
+    # train, test = train_test_split(df, test_size=test_size)
+    os.chdir(output_folder)
+    if model_type == 'reg':
+        if not metric:
+            metric = 'RMSE'
+        reg = pyreg.setup(target=y_col, data=df, normalize=normalize, train_size=1-test_size, fold=folds, silent=True)
+        best_model = pyreg.compare_models(sort=metric)
+        reg_model = pyreg.create_model(best_model)
+        reg_tuned_model = pyreg.tune_model(reg_model)
+        pyreg.plot_model(reg_tuned_model, save=True)
+        pyreg.plot_model(reg_tuned_model, plot='feature', save=True)
+        pyreg.plot_model(reg_tuned_model, plot='error', save=True)
+        final_model = pyreg.finalize_model(reg_tuned_model)
+        if testing_set:
+            unseen_predictions = pyreg.predict_model(final_model, data=test)
+            r2 = check_metric(unseen_predictions[y_col], unseen_predictions.Label, 'R2')
+            rmse = check_metric(unseen_predictions[y_col], unseen_predictions.Label, 'RMSE')
+        pyreg.save_model(final_model, model_name)
+        pyreg.save_experiment('model_session')
+        with open('model.log', 'w') as f:
+            f.writelines("%s\n" % output for output in reg)
+    elif model_type == 'classifier':
+        if not metric:
+            metric = 'AUC'
+        classifier = cl.setup(target=y_col, fix_imbalance=imbalanced, data=df, train_size=1-test_size, silent=True)
+        best_model = cl.compare_models(sort=metric)
+        cl_model = cl.create_model(best_model)
+        cl_tuned_model = pyreg.tune_model(cl_model)
+        cl.plot_model(cl_tuned_model, plot='pr', save=True)
+        cl.plot_model(cl_tuned_model, plot='confusion_matrix', save=True)
+    return
