bececd9d43e64634274b1207742a95b4d03072bc
AldisiRana <35561773+AldisiRana@users.noreply.github.com>
2021-05-20 10:45:34 +0200

    Update plot_script.R

--- scripts/plot_script.R
+++ scripts/plot_script.R
@@ -1,4 +1,4 @@
-#! /usr/bin/Rscript
+#!/usr/bin/Rscript
 # if (!require("pacman")) install.packages("pacman", repos = "https://CRAN.R-project.org/")
 #pacman::p_load(data.table, qqman, ggplot2, grid, optparse, gridGraphics)
 
