5f0dde3b8fb189ba7cb7d1dc1aeb4ac0274e86b4
AldisiRana <35561773+AldisiRana@users.noreply.github.com>
2021-06-29 00:01:57 +0200

    Update helpers.py

--- src/genrisk/helpers.py
+++ src/genrisk/helpers.py
@@ -129,7 +129,7 @@ def get_pvals_logit(*, df, genes, cases_column, **kwargs):
 
 
 def get_pvals_linear(*, df, genes, cases_column, **kwargs):
-    covariates = kwargs['covariates']
+    covariates = kwargs.get('covariates')
     y_set = df[[cases_column]]
     x_set = df[covariates]
     genes_df = df[genes]
