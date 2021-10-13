51896e9a8e7a1f0acd4441a5689c7921106f5ff6
Rana Aldisi <aldisi.rana@gmail.com>
2021-10-13 12:43:16 +0200

    Update gene_scoring.py

--- src/genrisk/gene_scoring.py
+++ src/genrisk/gene_scoring.py
@@ -78,9 +78,8 @@ def get_gene_info(
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
     gene_file = output_dir + '.genes'
-    f = open(os.path.join(output_dir, gene_file), 'w')
-    f.writelines("%s\n" % gene for gene in genes)
-    f.close()
+    with open(gene_file, 'w') as f:
+        f.writelines("%s\n" % gene for gene in genes)
     [df[df[genes_col] == gene][[variant_col, alt_col, 'score', genes_col]].to_csv(os.path.join(output_dir, (
         str(gene) + '.w')), index=False, sep='\t') for gene in tqdm(genes, desc="writing w gene files")]
     [df[df[genes_col] == gene][[variant_col, alt_col]].to_csv(os.path.join(output_dir, (str(gene) + '.v')),
