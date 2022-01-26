452743d135f7eda60e3f0583d6c67d6b93e5e8e7
Rana Aldisi <aldisi.rana@gmail.com>
2022-01-26 14:54:45 +0100

    update docstrings in pipeline

--- src/genrisk/pipeline.py
+++ src/genrisk/pipeline.py
@@ -40,24 +40,44 @@ def scoring_process(
 ):
     """
     Calculate gene-based scores.
-    \f
+    This is calculated by a weighted sum of the variants in a gene.
 
-    :param bfiles: the binary files for plink process.
-    :param logger: an object that logs function outputs.
-    :param annotated_vcf: an annotated containing variant IDs, alt, info and samples genotypes.
-    :param plink: the directory of plink, if not set in environment
-    :param beta_param: the parameters from beta weight function.
-    :param temp_dir: a temporary directory to save temporary files before merging.
-    :param output_file: the final output scores matrix.
-    :param weight_func: the weighting function used in score calculation.
-    :param variant_col: the column containing the variant IDs.
-    :param gene_col: the column containing gene names.
-    :param af_col: the column containing allele frequency.
-    :param del_col: the column containing deleteriousness score.
-    :param alt_col: the column containing alternate base.
-    :param maf_threshold: the threshold for minor allele frequency.
+    Parameters
+    ----------
+    logger
+        an object that logs function outputs.
+    annotated_vcf : str
+        an annotated containing variant IDs, alt, info and samples genotypes.
+    temp_dir : str
+        a temporary directory to save temporary files before merging.
+    beta_param : tuple
+        the parameters from beta weight function. ignore if log10 function is chosen.
+    weight_func : str
+        the weighting function used in score calculation.
+    del_col : str
+        the column containing deleteriousness score or functional annotation.
+    maf_threshold : float
+        the threshold for minor allele frequency. between [0.0-1.0]
+    gene_col : str
+        the column containing gene names.
+    variant_col : str
+        the column containing variant IDs.
+    af_col : str
+        the column containing allele frequency.
+    alt_col : str
+        the column containing alternate allele.
+    bfiles : str
+        the binary files for plink process. if vcf contains all information bfiles are not needed.
+    plink : str
+        the directory of plink, if not set in environment
+    output_file : str
+        the path to save the final output scores matrix.
 
-    :return: gene-based scores dataframe.
+    Returns
+    -------
+    DataFrame
+        final scores matrix
+
     """
     try:
         genes_folder = get_gene_info(
@@ -108,24 +128,42 @@ def find_pvalue(
     logger
 ):
     """
-    Calculate the significance of a gene in a population using Mann-Whitney-U test.
-    \f
+    Calculate the significance of a gene in a population using different statistical analyses [mannwhitneyu, logit, linear, ttest_ind]
 
-    :param test: the type of statistical test to use, choices are: t-test, mannwhitenyu, GLM, logit.
-    :param scores_file: dataframe containing the scores of genes across samples.
-    :param info_file: a file containing the information of the sample.
-    :param output_file: a path to save the output file.
-    :param genes: a list of the genes to calculate the significance. if None will calculate for all genes.
-    :param cases_column: the name of the column containing cases and controls information.
-    :param samples_column: the name of the column contining samples IDs.
-    :param adj_pval: the method for pvalue adjustment.
-    :param controls: the name of the controls category.
-    :param cases:  the name of the cases category.
-    :param covariates: the covariates of the phenotype.
-    :param processes: number of processes working in parallel.
-    :param logger: an object that logs function outputs.
+    Parameters
+    ----------
+    scores_file : str
+        dataframe containing the scores of genes across samples.
+    info_file : str
+        a file containing the information of the sample. this includes target phenotype and covariates.
+    output_file : str
+         a path to save the summary statistics.
+    genes : list
+        a list of the genes to calculate the significance. if None will calculate for all genes.
+    cases_column : str
+        the name of the column containing phenotype information.
+    samples_column : str
+        the name of the column contining samples IDs.
+    test : str
+        the type of statistical test to use, choices are: ttest_ind, mannwhitenyu, linear, logit.
+    adj_pval : str
+        the method for pvalue adjustment. Choices: ['bonferroni', 'sidak', 'holm-sidak', 'holm', 'simes-hochberg', 'hommel', 'fdr_bh', 'fdr_by', 'fdr_tsbh', 'fdr_tsbky']
+    covariates : str
+        the list of covariates used in the calculation.
+    cases : str
+        the cases category. if binary phenotype.
+    controls : str
+        the controls category. if binary phenotype.
+    processes : int
+        number of processes used, for parallel computing.
+    logger
+        an object that logs function outputs.
 
-    :return: dataframe with genes and their p_values
+    Returns
+    -------
+    DataFrame
+        dataframe with genes and their p_values
+
     """
     logger.info("Reading scores file...")
     if genes:
@@ -183,19 +221,33 @@ def betareg_pvalues(
     logger,
 ):
     """
-    Calculate association significance between two groups using betareg.
-    \f
+    Calculate association significance using betareg. This function runs in Rscript.
 
-    :param scores_file: the path to the scores file.
-    :param pheno_file:  the path to the phenotypes and covariates file.
-    :param samples_col: the name of the column containing the samples IDs.
-    :param cases_col: the name of the column containing the case/controls.
-    :param output_path: the path to the output file.
-    :param covariates: the covariates used in calculations, written with no space and comma in between (e.g PC1,PC2)
-    :param processes: number of processes to parallelize.
-    :param logger: an object that logs function outputs.
 
-    :return:
+    Parameters
+    ----------
+    scores_file : str
+        the path to the scores file.
+    pheno_file : str
+        the path to the phenotypes and covariates file.
+    samples_col : str
+        column containing samples ids.
+    cases_col : str
+        column containing the phenotype.
+    output_path : str
+        a path to save the summary statistics.
+    covariates : str
+        the list of covariates used in the calculation.
+    processes : int
+        number of processes used, for parallel computing.
+    genes : str
+        a list of the genes to calculate the significance. if None will calculate for all genes.
+    logger
+        an object that logs function outputs.
+
+    Returns
+    -------
+
     """
     logger.info("The p_values will be calculated using beta regression.")
     try:
@@ -230,22 +282,44 @@ def create_prediction_model(
     metric=None,
     seed,
     include_models,
+    normalize_method,
 ):
     """
     Create a prediction model (classifier or regressor) using the provided dataset.
-    \f
 
-    :param model_name: the name of the prediction model.
-    :param model_type: type of model (reg or classifier).
-    :param y_col: the column containing the target (qualitative or quantitative).
-    :param imbalanced: True means data is imbalanced.
-    :param normalize: True if data needs normalization.
-    :param folds: how many folds for cross-validation.
-    :param training_set: the training set for the model.
-    :param testing_set: if exists an extra evaluation step will be done using the testing set.
-    :param test_size: the size to split the training/testing set.
-    :param metric: the metric to evaluate the best model.
-    :return: the metrics.
+    Parameters
+    ----------
+    model_name : str
+        the name of the prediction model.
+    model_type : str
+        type of model [regressor|classifier]
+    y_col : str
+        the column containing the target (qualitative or quantitative).
+    imbalanced : bool
+        True means data is imbalanced.
+    normalize : bool
+        True if data needs normalization.
+    folds : int
+        how many folds for cross-validation.
+    training_set : pd.DataFrame
+        the training set for the model.
+    testing_set : pd.DataFrame
+        if exists an extra evaluation step will be done using the testing set.
+    test_size : float
+         the size to split the training set for cross-validation.
+    metric : str
+        the metric to evaluate the best model.
+    seed : int
+        the intilization state random number
+    include_models : list
+        a list of models that the user wants to test. if None all models will be used.
+    normalize_method : str
+        the method to normalize the data. Choices [zscore, minmax, maxabs, robust]
+
+    Returns
+    -------
+    Final model
+
     """
     try:
         model_func = {'classifier': classification_model, 'regressor': regression_model}
@@ -261,6 +335,7 @@ def create_prediction_model(
             imbalanced=imbalanced,
             seed=seed,
             include_models=include_models,
+            normalize_method=normalize_method
         )
     except Exception:
         raise Exception('Model requested is not available. Please choose regressor or classifier.')
@@ -276,16 +351,26 @@ def model_testing(
     model_type
 ):
     """
-    Load a prediction model and use it to predict label values in a dataset.
-    \f
+    Load a prediction model and use it to predict label values in an independent dataset.
 
-    :param model_path: the path to saved model.
-    :param input_file: the file with features and label.
-    :param samples_col: the name of samples column.
-    :param label_col: the name of the target column.
-    :param model_type: regressor or classifier
+    Parameters
+    ----------
+    model_path : str
+        the path to saved model.
+    input_file : str
+        the file with the test dataset.
+    samples_col : str
+        the name of samples column.
+    label_col : str
+        the name of the target column
+    model_type :
+        regressor or classifier
 
-    :return: a dataframe with prediction values.
+    Returns
+    -------
+    DataFrame
+        the results of predictions.
+
     """
     model = joblib.load(model_path)
     testing_df = pd.read_csv(input_file, sep='\t', index_col=samples_col)
