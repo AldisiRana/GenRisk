047fa550e65eafa7f6c5065ebca6f034f9a2f1b8
Rana Aldisi <aldisi.rana@gmail.com>
2022-01-31 15:39:03 +0100

    fix docs

--- src/genrisk/cli.py
+++ src/genrisk/cli.py
@@ -60,7 +60,16 @@ def score_genes(
         maf_threshold,
 ):
     """
-    Calculate the gene-based scores for a given dataset. \f
+    Calculate the gene-based scores for a given dataset.
+
+    Example
+    ---------
+    This function is performed using commandline interface::
+
+        $ genrisk score-genes --annotated-vcf annotated_vcf_toy.vcf --temp-dir test/
+        --output-file test.tsv --weight-func beta --maf-threshold 0.01 --alt-col ALT
+        --variant-col ID --af-col AF --del-col CADD --gene-col Gene
+    \f
 
     Parameters
     ----------
